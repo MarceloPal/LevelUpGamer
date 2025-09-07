e696cf3db44ad6e97452311d471e146be0ee1928
Areliz Isla <are.isla@duocuc.cl>
2025-09-07 15:49:06 -0300

    se agregan js perdidos para los slides y perfil de usuario

--- js/app.js
+++ js/app.js
@@ -44,3 +44,49 @@ function mostrarForgetPassword() {
     document.getElementById("forgetPasswordForm").classList.remove("d-none");
 }
   
+
+// Perfil de usuario
+const links = document.querySelectorAll('.profile-sidebar .nav-link');
+const sections2 = document.querySelectorAll('.profile-content .section');
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    // Quitar active de todos los links
+    links.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
+
+    // Mostrar la sección correspondiente
+    const target = link.getAttribute('data-section');
+    sections2.forEach(sec => {
+      if (sec.id === target) {
+        sec.classList.remove('d-none');
+      } else {
+        sec.classList.add('d-none');
+      }
+    });
+  });
+});
+
+// script SLIDES
+document.addEventListener('DOMContentLoaded', function () {
+	if (typeof Swiper !== 'undefined') {
+		var swiper = new Swiper(".mySwiper", {
+			spaceBetween: 30,
+			centeredSlides: true,
+			autoplay: {
+				delay: 2500,
+				disableOnInteraction: false,
+			},
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+		});
+	}
+});
