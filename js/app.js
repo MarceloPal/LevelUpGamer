1de550015c3ef8a425bbdafa823a6869174f5145
Areliz Isla <are.isla@duocuc.cl>
2025-09-06 20:35:49 -0400

    WIP: guardando cambios antes de actualizar main

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.catalogo-buttons .btn2');
+const sections = document.querySelectorAll('.catalogo-content .section');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // quitar active a todos los botones
+    buttons.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+
+    const target = btn.getAttribute('data-section');
+
+    if (target === "todo") {
+      // Mostrar todas las secciones
+      sections.forEach(sec => sec.classList.remove('d-none'));
+    } else {
+      // Mostrar solo la seleccionada
+      sections.forEach(sec => {
+        if (sec.id === target) {
+          sec.classList.remove('d-none');
+        } else {
+          sec.classList.add('d-none');
+        }
+      });
+    }
+  });
+});
