1e6ec71ecf07b1535316efbb830248c198f73242
Areliz Isla <are.isla@duocuc.cl>
2025-09-06 22:32:02 -0400

    arregle un problema de las paginas del catalogo F

--- js/app.js
+++ js/app.js
@@ -3,18 +3,16 @@ const sections = document.querySelectorAll('.catalogo-content .section');
 
 buttons.forEach(btn => {
   btn.addEventListener('click', () => {
-    // Activar el botón seleccionado
     buttons.forEach(b => b.classList.remove('active'));
     btn.classList.add('active');
-    // Mostrar la sección correspondiente
     const target = btn.getAttribute('data-section');
     sections.forEach(sec => {
       if (target === "todo") {
-        sec.classList.remove('d-none1');
+        sec.classList.remove('d-none');
       } else if (sec.id === target) {
-        sec.classList.remove('d-none1');
+        sec.classList.remove('d-none');
       } else {
-        sec.classList.add('d-none1');
+        sec.classList.add('d-none');
       }
     });
   });
