9b94066ba20c1b6690e05db89ee2de57bbe2f6da
Areliz Isla <are.isla@duocuc.cl>
2025-09-07 03:27:42 -0300

    se agregan los js perdidos para slides y perfil

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// Filtros de catálogo
+
 const buttons = document.querySelectorAll('.catalogo-buttons .btn2');
 const sections = document.querySelectorAll('.catalogo-content .section');
 
@@ -19,6 +21,7 @@ buttons.forEach(btn => {
 });
 
 
+// Formulario de login y registro
 
 function mostrarRegistro() {
   document.getElementById("loginForm").classList.add("d-none");
@@ -30,3 +33,49 @@ function mostrarLogin() {
   document.getElementById("loginForm").classList.remove("d-none");
 }
   
+
+// Perfil de usuario
+const links = document.querySelectorAll('.profile-sidebar .nav-link');
+const sections2 = document.querySelectorAll('.profile-content .section');
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    // Quitar active de todos los links
+    links.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
+
+    // Mostrar la sección correspondiente
+    const target = link.getAttribute('data-section');
+    sections2.forEach(sec => {
+      if (sec.id === target) {
+        sec.classList.remove('d-none');
+      } else {
+        sec.classList.add('d-none');
+      }
+    });
+  });
+});
+
+// script SLIDES
+document.addEventListener('DOMContentLoaded', function () {
+	if (typeof Swiper !== 'undefined') {
+		var swiper = new Swiper(".mySwiper", {
+			spaceBetween: 30,
+			centeredSlides: true,
+			autoplay: {
+				delay: 2500,
+				disableOnInteraction: false,
+			},
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+		});
+	}
+});
