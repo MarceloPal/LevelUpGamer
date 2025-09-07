08cafe318c9b8f5371f415b4172f83ebaf3a0a51
Areliz Isla <are.isla@duocuc.cl>
2025-09-06 22:27:26 -0400

    me faltan cosas por agregar en el catalogo

--- js/app.js
+++ js/app.js
@@ -1,36 +1,34 @@
-
-  function mostrarRegistro() {
-    document.getElementById("loginForm").classList.add("d-none");
-    document.getElementById("registerForm").classList.remove("d-none");
-  }
-
-  function mostrarLogin() {
-    document.getElementById("registerForm").classList.add("d-none");
-    document.getElementById("loginForm").classList.remove("d-none");
-  }
-  const buttons = document.querySelectorAll('.catalogo-buttons .btn2');
+const buttons = document.querySelectorAll('.catalogo-buttons .btn2');
 const sections = document.querySelectorAll('.catalogo-content .section');
 
 buttons.forEach(btn => {
   btn.addEventListener('click', () => {
-    // quitar active a todos los botones
+    // Activar el botón seleccionado
     buttons.forEach(b => b.classList.remove('active'));
     btn.classList.add('active');
-
+    // Mostrar la sección correspondiente
     const target = btn.getAttribute('data-section');
-
-    if (target === "todo") {
-      // Mostrar todas las secciones
-      sections.forEach(sec => sec.classList.remove('d-none'));
-    } else {
-      // Mostrar solo la seleccionada
-      sections.forEach(sec => {
-        if (sec.id === target) {
-          sec.classList.remove('d-none');
-        } else {
-          sec.classList.add('d-none');
-        }
-      });
-    }
+    sections.forEach(sec => {
+      if (target === "todo") {
+        sec.classList.remove('d-none1');
+      } else if (sec.id === target) {
+        sec.classList.remove('d-none1');
+      } else {
+        sec.classList.add('d-none1');
+      }
+    });
   });
 });
+
+
+
+function mostrarRegistro() {
+  document.getElementById("loginForm").classList.add("d-none");
+  document.getElementById("registerForm").classList.remove("d-none");
+}
+
+function mostrarLogin() {
+  document.getElementById("registerForm").classList.add("d-none");
+  document.getElementById("loginForm").classList.remove("d-none");
+}
+  
